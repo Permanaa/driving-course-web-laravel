a82e226f37752b1b7a841eff8ef56bb47224e4f6
permanaa <irmanpermana.lu@gmail.com>
2023-09-04 00:34:22 +0700

    fix typo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -277,7 +277,7 @@
     <div class="bg-white py-24 sm:py-32">
       <div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
         <div class="max-w-2xl">
-          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Beberap pilihan mobil</h2>
+          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Beberapa pilihan mobil</h2>
         </div>
         <ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
           <li>
